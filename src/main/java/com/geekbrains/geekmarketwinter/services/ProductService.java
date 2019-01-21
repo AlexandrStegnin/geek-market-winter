abad8c01eddbd0b070ba40803e88e15936a4e789
aleksandrstegnin <alexandr.stegnin@gmail.com>
2019-01-22 00:05:02 +0500

    Добавлены поддержка спецификации репозиторием. Реализован метод в сервисе, который возвращает страницу (page). Добавлен maven плагин для генерации Jpa Metamodel.

--- src/main/java/com/geekbrains/geekmarketwinter/services/ProductService.java
+++ src/main/java/com/geekbrains/geekmarketwinter/services/ProductService.java
@@ -2,26 +2,33 @@ package com.geekbrains.geekmarketwinter.services;
 
 import com.geekbrains.geekmarketwinter.entites.Product;
 import com.geekbrains.geekmarketwinter.repositories.ProductRepository;
+import com.geekbrains.geekmarketwinter.services.specifications.ProductSpecification;
+import com.geekbrains.geekmarketwinter.utils.filters.ProductFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ProductService {
-    private ProductRepository productRepository;
+
+    private final ProductRepository productRepository;
+    private final ProductSpecification specification;
 
     @Autowired
-    public void setProductRepository(ProductRepository productRepository) {
+    public ProductService(ProductSpecification specification, ProductRepository productRepository) {
+        this.specification = specification;
         this.productRepository = productRepository;
-    }
-
-    public List<Product> getAllProducts() {
-        return (List<Product>)(productRepository.findAll());
     }
 
     public Product getProductById(Long id) {
         return productRepository.findById(id).get();
+    }
+
+    public Page<Product> findAll(ProductFilter filters, Pageable pageable) {
+        return productRepository.findAll(
+                specification.getFilter(filters),
+                pageable
+        );
     }
 }
