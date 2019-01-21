3477de9b1291da1e4706232dbc96c57414af7379
aleksandrstegnin <alexandr.stegnin@gmail.com>
2019-01-22 00:00:41 +0500

    Добавлены спецификации для фильтрации данных.

---
+++ src/main/java/com/geekbrains/geekmarketwinter/services/specifications/ProductSpecification.java
@@ -0,0 +1,33 @@
+package com.geekbrains.geekmarketwinter.services.specifications;
+
+import com.geekbrains.geekmarketwinter.entites.Product;
+import com.geekbrains.geekmarketwinter.entites.Product_;
+import com.geekbrains.geekmarketwinter.utils.filters.ProductFilter;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
+import static org.springframework.data.jpa.domain.Specification.where;
+
+@Component
+public class ProductSpecification extends BaseSpecification<Product, ProductFilter> {
+    @Override
+    public Specification<Product> getFilter(ProductFilter filter) {
+        return (root, query, cb) -> where(
+                titleLike(filter.getTitle()))
+                .toPredicate(root, query, cb);
+    }
+
+    private static Specification<Product> titleLike(String title) {
+        return ((root, criteriaQuery, criteriaBuilder) -> {
+            if (Objects.equals(null, title) || StringUtils.isEmpty(title)) {
+                return null;
+            } else {
+                return criteriaBuilder.like(root.get(Product_.title), title);
+            }
+        }
+        );
+    }
+}
