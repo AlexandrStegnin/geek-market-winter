01ff263685651b882d52c4b09d6a067759eefe7a
aleksandrstegnin <alexandr.stegnin@gmail.com>
2019-02-11 23:21:10 +0500

    Добавлено удаление категорий товаров. Мелкий рефакторинг.

--- src/main/java/com/geekbrains/geekmarketwinter/vaadin/ui/admin/CategoryView.java
+++ src/main/java/com/geekbrains/geekmarketwinter/vaadin/ui/admin/CategoryView.java
@@ -5,11 +5,14 @@ import com.geekbrains.geekmarketwinter.services.AuthService;
 import com.geekbrains.geekmarketwinter.services.CategoryService;
 import com.geekbrains.geekmarketwinter.vaadin.custom.CustomAppLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.editor.Editor;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
@@ -32,16 +35,20 @@ public class CategoryView extends VerticalLayout {
     private Grid<Category> grid;
     private AuthService auth;
     private ListDataProvider<Category> dataProvider;
+    private Button addNewBtn;
 
     public CategoryView(CategoryService categoryService, AuthService auth) {
         this.categoryService = categoryService;
         this.dataProvider = new ListDataProvider<>(getAllCategories());
         this.grid = new Grid<>();
         this.auth = auth;
+        this.addNewBtn = new Button("Add new category", e -> showDialog());
         init();
     }
 
     private void init() {
+        addNewBtn.setIcon(VaadinIcon.PLUS.create());
+        addNewBtn.setIconAfterText(true);
 
         grid.setDataProvider(dataProvider);
 
@@ -79,11 +86,16 @@ public class CategoryView extends VerticalLayout {
         descriptionField.setSizeFull();
 
         Grid.Column<Category> editorColumn = grid.addComponentColumn(category -> {
-            Button edit = new Button("Edit");
-            edit.addClassName("edit");
+            Div actions = new Div();
+            Button edit = new Button("", VaadinIcon.EDIT.create());
             edit.addClickListener(e -> editor.editItem(category));
-            return edit;
+            Button delete = new Button("", VaadinIcon.TRASH.create());
+            delete.addClickListener(e -> showDeleteDialog(category));
+            actions.add(edit, delete);
+            return actions;
         });
+        editorColumn.setHeader("Actions");
+        editorColumn.setKey("actions");
 
         Button save = new Button("Save", e -> editor.save());
         save.addClassName("save");
@@ -99,7 +111,11 @@ public class CategoryView extends VerticalLayout {
 
         Notification message = new Notification("", 3000, Notification.Position.TOP_END);
 
-        CustomAppLayout appLayout = new CustomAppLayout(auth, grid);
+        VerticalLayout verticalLayout = new VerticalLayout();
+
+        verticalLayout.add(addNewBtn, grid);
+        verticalLayout.setAlignItems(Alignment.END);
+        CustomAppLayout appLayout = new CustomAppLayout(auth, verticalLayout);
         add(appLayout);
         setHeight("100vh");
 
@@ -113,10 +129,75 @@ public class CategoryView extends VerticalLayout {
                     "Description = " + updatedCategory.getDescription());
             message.open();
         });
+
+        grid.getColumnByKey("actions").setTextAlign(ColumnTextAlign.CENTER);
+
+    }
+
+    private void deleteCategory(Category category) {
+        dataProvider.getItems().remove(category);
+        categoryService.delete(category);
+        dataProvider.refreshAll();
+    }
+
+    private void showDialog() {
+        Dialog dialog = new Dialog();
+        TextField title = new TextField("Title");
+        title.setPlaceholder("Enter title");
+        TextField description = new TextField("Description");
+        description.setPlaceholder("Enter description");
+        HorizontalLayout fields = new HorizontalLayout();
+        fields.add(title, description);
+
+        dialog.setCloseOnEsc(false);
+        dialog.setCloseOnOutsideClick(false);
+        Button save = new Button("Save", e -> {
+            Category category = new Category();
+            category.setTitle(title.getValue());
+            category.setDescription(description.getValue());
+            addNewCategory(category);
+            dialog.close();
+        });
+        Button cancel = new Button("Cancel", e -> dialog.close());
+        HorizontalLayout actions = new HorizontalLayout();
+        actions.add(save, cancel);
+
+        VerticalLayout content = new VerticalLayout();
+        content.add(fields, actions);
+        dialog.add(content);
+        dialog.open();
+        title.getElement().callFunction("focus");
+    }
+
+    private void showDeleteDialog(Category category) {
+        Dialog dialog = new Dialog();
+        Div contentText = new Div();
+        contentText.setText("Are you sure, you want to delete category: \n" + category.getTitle() + "?");
+
+        dialog.setCloseOnEsc(false);
+        dialog.setCloseOnOutsideClick(false);
+        Button yes = new Button("Yes", e -> {
+            deleteCategory(category);
+            dialog.close();
+        });
+        Button no = new Button("No", e -> dialog.close());
+        HorizontalLayout actions = new HorizontalLayout();
+        actions.add(yes, no);
+
+        VerticalLayout content = new VerticalLayout();
+        content.add(contentText, actions);
+        dialog.add(content);
+        dialog.open();
     }
 
     private List<Category> getAllCategories() {
         return categoryService.getAllCategories();
+    }
+
+    private void addNewCategory(Category category) {
+        category = categoryService.create(category);
+        dataProvider.getItems().add(category);
+        dataProvider.refreshAll();
     }
 
 }
