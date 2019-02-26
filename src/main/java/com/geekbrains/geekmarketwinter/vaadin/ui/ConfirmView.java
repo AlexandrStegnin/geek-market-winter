9fcb3be70225ab7ef447783996d808a2082727bd
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-02-26 10:29:57 +0500

    Сбрасываем корзину после подтверждения заказа

--- src/main/java/com/geekbrains/geekmarketwinter/vaadin/ui/ConfirmView.java
+++ src/main/java/com/geekbrains/geekmarketwinter/vaadin/ui/ConfirmView.java
@@ -6,6 +6,7 @@ import com.geekbrains.geekmarketwinter.entites.Order;
 import com.geekbrains.geekmarketwinter.repositories.AuthRepository;
 import com.geekbrains.geekmarketwinter.services.DeliveryAddressService;
 import com.geekbrains.geekmarketwinter.services.OrderService;
+import com.geekbrains.geekmarketwinter.services.ShoppingCartService;
 import com.geekbrains.geekmarketwinter.vaadin.custom.CustomAppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -19,6 +20,7 @@ import com.vaadin.flow.data.binder.BindingValidationStatus;
 import com.vaadin.flow.function.SerializablePredicate;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.material.Material;
 
@@ -36,21 +38,23 @@ public class ConfirmView extends VerticalLayout {
     private final OrderService orderService;
     private final DeliveryAddressService addressService;
     private final OrderMessageSender orderMessageSender;
+    private final ShoppingCartService cartService;
     private String delivAddress;
 
     public ConfirmView(AuthRepository auth,
                        OrderService orderService,
                        DeliveryAddressService addressService,
-                       OrderMessageSender orderMessageSender) {
+                       OrderMessageSender orderMessageSender,
+                       ShoppingCartService cartService) {
         this.orderMessageSender = orderMessageSender;
         this.addressService = addressService;
         this.orderService = orderService;
+        this.cartService = cartService;
         this.auth = auth;
         init();
     }
 
     private void init() {
-        // TODO: 2019-02-25 Сбрасывать данные корзины
         FormLayout formLayout = new FormLayout();
         Binder<Order> binder = new Binder<>();
 
@@ -108,7 +112,7 @@ public class ConfirmView extends VerticalLayout {
                 Notification notification = new Notification("Order have been confirmed", 3000,
                         Notification.Position.TOP_END);
                 notification.open();
-
+                cartService.resetCart(VaadinRequest.getCurrent());
                 getUI().ifPresent(ui -> ui.navigate(SHOP_PAGE));
             } else {
                 BinderValidationStatus<Order> validate = binder.validate();
